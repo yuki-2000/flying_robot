5419fe4f7aa6321dcfcd63b1743c39d37e640904
yuki-2000 <88224293+yuki-2000@users.noreply.github.com>
2023-06-03 15:36:27 +0900

    v20211122

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,33 +1,99 @@
 #include "controller.hpp"
 #include "parameters.hpp"
+#include    <math.h>
 
 // controller.cpp内の初期化を行う関数
 void controller_init() {
-    // 変数の初期化
+
+
 }
+
+
+
+
+// 変数の初期化
+//xyzの順
+float hight =1.0;
+float velocity[3]= {0,0,0};
+float position[3]= {0,0,hight};
+//pitch,yaw,roll　1ステップ前の値を保存しておきたい
+//error0が一回前、error1が今回の角度差分
+//pitch yaw rollの順
+float error0[3]= {0,0,0};
+float error1[3]= {0,0,0};
+float integral[3]={0,0,0};
+
+float KP[3]={10,10,10};
+float KI[3]={10,10,10};
+float KD[3]={10,10,10};
+
+float p[3]={0,0,0};
+float i[3]={0,0,0};
+float d[3]={0,0,0};
+
 
 // 制御量を計算する関数
 // 0.1 s間隔で呼び出される
 // この関数内ではdelayなど処理時間のかかる処理は行わない
 OutputInfo calc_output(const SensorInfo& sensors) {
     OutputInfo outputs;
+
+
+
+
+
     // 経過時間[ms]を経過時間[s]に変換
     int seconds = sensors.time_ms / 1000;
 
-    if (seconds % 2 == 0) {
-        // エレベーターとラダーの角度
-        outputs.servo_angle_elevator = 0;
-        outputs.servo_angle_rudder = 0;
+    //進行方向と機体向きの角度差をとりたい
+    //進行方向の速度ベクトルを知りたいが、加速度ベクトルしかないので積分する
+    velocity[0] += sensors.accel[0]* WAIT_TIME_MS;
+    velocity[1] += sensors.accel[1]* WAIT_TIME_MS;
+    velocity[2] += sensors.accel[2]* WAIT_TIME_MS;
 
-        // 秒数が偶数ならLEDを点灯させる
-        outputs.led_on = true;
-    } else {
-        // エレベーターとラダーの角度
-        outputs.servo_angle_elevator = 10;
-        outputs.servo_angle_rudder = 10;
 
-        // 秒数が奇数ならLEDを消灯させる
-        outputs.led_on = false;
+    position[0] += velocity[0]* WAIT_TIME_MS;
+    position[1] += velocity[1]* WAIT_TIME_MS;
+    position[2] += velocity[2]* WAIT_TIME_MS;
+
+    //内積から角度を求める.ピッチはdgreeなので合わせる。
+    float velo_pitch = acos(velocity[0]/sqrt(velocity[0]*velocity[0] + velocity[3]*velocity[3])) * 180/M_PI;
+    float velo_yaw = acos(velocity[0]/sqrt(velocity[0]*velocity[0] + velocity[2]*velocity[2])) * 180/M_PI;
+    float velo_roll = acos(velocity[1]/sqrt(velocity[1]*velocity[1] + velocity[2]*velocity[2])) * 180/M_PI;
+
+   //前の値を保存しておく
+    error0[0] = error1[0];
+    error0[1] = error1[1];
+    error0[2] = error1[2];
+
+    error1[0] = sensors.pitch + 5 - velo_pitch;
+    error1[1] = 0 - velo_yaw;
+    error1[2] = 0 - velo_roll;
+   
+
+    integral[0] += (error1[0] + error0[0]) / 2.0 * WAIT_TIME_MS;
+    integral[1] += (error1[1] + error0[1]) / 2.0 * WAIT_TIME_MS;
+    integral[2] += (error1[2] + error0[2]) / 2.0 * WAIT_TIME_MS;
+
+    p[0] = KP[0] * error1[0];
+    p[1] = KP[1] * error1[1];
+    p[2] = KP[2] * error1[2];
+
+    i[0] = KI[0] * integral[0];
+    i[1] = KI[1] * integral[1];
+    i[2] = KI[2] * integral[2];
+
+    d[0]= KD[0]*(error1[0]-error1[0])/WAIT_TIME_MS;
+    d[1]= KD[1]*(error1[1]-error1[1])/WAIT_TIME_MS;
+    d[2]= KD[2]*(error1[2]-error1[2])/WAIT_TIME_MS;
+
+    if (sensors.pitch >15){
+        outputs.servo_angle_elevator = 30;
+    }else{
+        outputs.servo_angle_elevator = p[0] + i[0] + d[0];
     }
+    outputs.servo_angle_rudder = p[1] + i[1] + d[1];
+
+    
     return outputs;
 }
