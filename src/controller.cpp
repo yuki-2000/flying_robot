d79c522ffa76b88ae7fc35fd4002312776ae53f3
yuki-2000 <88224293+yuki-2000@users.noreply.github.com>
2023-06-03 15:38:17 +0900

    v20211226

--- src/controller.cpp
+++ src/controller.cpp
@@ -17,19 +17,21 @@ float position[3]= {0,0,hight};
 //pitch,yaw,roll　1ステップ前の値を保存しておきたい
 //error0が一回前、error1が今回の角度差分
 //pitch yaw rollの順
-float error0[3]= {0,0,0};
-float error1[3]= {0,0,0};
-float integral[3]={0,0,0};
+double error0[3]= {0,0,0};
+double error1[3]= {0,0,0};
+double integral[3]={0,0,0};
 
-float KP[3]={2,2,2};
-float KI[3]={2,2,2};
-float KD[3]={2,2,2};
+double KP[3]={2,2,2};
+double KI[3]={2,2,2};
+double KD[3]={0.02,0.02,0.02};
 
-float p[3]={0,0,0};
-float i[3]={0,0,0};
-float d[3]={0,0,0};
+double p[3]={0,0,0};
+double i[3]={0,0,0};
+double d[3]={0,0,0};
 
-float stable_value[3]= {0,0,0};
+double stable_value[3]= {0,0,0};
+//元がintなので0になって後々0除算になってしまう
+double wait_s = WAIT_TIME_MS/1000;
 
 
 // 制御量を計算する関数
@@ -85,21 +87,20 @@ OutputInfo calc_output(const SensorInfo& sensors) {
     error0[0] = error1[0];
     error0[1] = error1[1];
     error0[2] = error1[2];
-
     //error1[0] = sensors.pitch + 5 - velo_pitch;
     //error1[1] = 0 - velo_yaw;
     //error1[2] = 0 - velo_roll;
 
-    error1[0] = stable_value[0] + 5-sensors.pitch ;
+    error1[0] = stable_value[0] + 0-sensors.pitch ;
     error1[1] = stable_value[1] + 0 - sensors.yaw;
     error1[2] = stable_value[2] + 0 - sensors.roll;
 
 
    
-
-    integral[0] += (error1[0] + error0[0]) / 2.0 * WAIT_TIME_MS/1000;
-    integral[1] += (error1[1] + error0[1]) / 2.0 * WAIT_TIME_MS/1000;
-    integral[2] += (error1[2] + error0[2]) / 2.0 * WAIT_TIME_MS/1000;
+    //0.02はwait_MSのs表示、式中に変数で入れるとなぜか0になって0除算に
+    integral[0] += ((error1[0] + error0[0]) / 2.0) * 0.02;
+    integral[1] += ((error1[1] + error0[1]) / 2.0) * 0.02;
+    integral[2] += ((error1[2] + error0[2]) / 2.0) * 0.02;
 
     p[0] = KP[0] * error1[0];
     p[1] = KP[1] * error1[1];
@@ -109,32 +110,47 @@ OutputInfo calc_output(const SensorInfo& sensors) {
     i[1] = KI[1] * integral[1];
     i[2] = KI[2] * integral[2];
 
-    d[0]= KD[0]*(error1[0]-error1[0])*1000/WAIT_TIME_MS;
-    d[1]= KD[1]*(error1[1]-error1[1])*1000/WAIT_TIME_MS;
-    d[2]= KD[2]*(error1[2]-error1[2])*1000/WAIT_TIME_MS;
+    d[0]= KD[0]*(error1[0]-error0[0])/0.02;
+    d[1]= KD[1]*(error1[1]-error0[1])/0.02;
+    d[2]= KD[2]*(error1[2]-error0[2])/0.02;
 
     if (sensors.pitch >15){
         outputs.servo_angle_elevator = 30;
         //outputs.servo_angle_rudder = 30;
     }else{
         outputs.servo_angle_elevator = p[0] + i[0] + d[0];
-        outputs.servo_angle_rudder = p[1] + i[1] + d[1];
     }
-    //outputs.servo_angle_rudder = p[1] + i[1] + d[1];
+    outputs.servo_angle_rudder = p[1] + i[1] + d[1];
 
 
+
+    char er0[256];
+    sprintf(er0,"error0 pitch: %lf, yaw: %lf, roll: %lf",error0[0], error0[1], error0[2]);
+    Serial.println(er0);  
 
     char er1[256];
     sprintf(er1,"error1 pitch: %lf, yaw: %lf, roll: %lf",error1[0], error1[1], error1[2]);
     Serial.println(er1);   
 
+    
+
+    Serial.println(WAIT_TIME_MS);
+    Serial.println(wait_s);
+
+
+    char pidp[256];
+    sprintf(pidp,"pitch P: %lf, I: %lf, D: %lf, 合計%lf",p[0], i[0], d[0], p[0] + i[0] + d[0]);
+    Serial.println(pidp);
+
+   char pidy[256];
+    sprintf(pidy,"yaw P: %lf, I: %lf, D: %lf, 合計%lf",p[1], i[1], d[1], p[1] + i[1] + d[1]);
+    Serial.println(pidy);
+
     char pidr[256];
-    sprintf(pidr,"roll P: %lf, I: %lf, D: %lf, 合計%lf",p[0], i[0], d[0], p[0] + i[0] + d[0]);
+    sprintf(pidr,"roll P: %lf, I: %lf, D: %lf, 合計%lf",p[2], i[2], d[2], p[2] + i[2] + d[2]);
     Serial.println(pidr);
 
-    char pidy[256];
-    sprintf(pidy,"yaw P: %lf, I: %lf, D: %lf　合計%lf",p[1], i[1], d[1], p[1] + i[1] + d[1]);
-    Serial.println(pidy);
+ 
 
 
 }
